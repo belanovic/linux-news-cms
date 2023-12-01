0e1a2f473be6f67f2a6ac32097a1081185471e75
Goran Belanovic <goranbelanovic@yahoo.com>
2023-12-01 16:41:08 +0100

    msg

--- src/Proba1.js
+++ src/Proba1.js
@@ -1,15 +1,63 @@
+
 import React, {useState} from 'react'
 
 export default function Proba1() {
 
     const [odgovor, setOdgovor] = useState([]);
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
+
+    const [username, setUsername]  = useState('');
+    const [password, setPassword]  = useState('');
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        e.target.name == "username"? setUsername(value) : setPassword(value)
+    }
+
+    const register = async (e) => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json' 
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        }
+        const res = await fetch('http://localhost:2000/register', options);
+        const body = await res.json();
+        /* console.log(`response od register fetcha: ${payload}`); */
+        console.log(body);
+    }
+
+    const login = async () => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'                
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        };
+        const res = await fetch('http://localhost:2000/login', options);
+        const login_message = await res.json();
+        if(login_message.logged_in) {
+            console.log(`You are logged in. You token is ${login_message.token}`);
+            localStorage.setItem('token', login_message.token);
+        } else {
+            console.log(login_message.error_message);
+        }
+        
+    }
 
     const sendRequest = async () => {
         const options = {
             method: 'GET',
             headers: {
-                'Authorization' : 'Bearer fsdfsadwerqwrsdsdcsdfsdrfwefsdfsdgsdg'
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
             }
         }
         const res = await fetch('http://localhost:2000/find', options);
@@ -17,7 +65,7 @@ export default function Proba1() {
         console.log(data)
         setOdgovor(data)
     }
-    console.log('pozvana komponenta funkcija')
+    /* console.log('pozvana komponenta funkcija') */
 
     return (
         <div className = "proba1" style = {{
@@ -25,6 +73,54 @@ export default function Proba1() {
             background: "black"
         }}>
             <h1>Proba1</h1>
+            <div className='register'>
+                <span>Username</span>
+                <input
+                    className = "username"
+                    name = "username"
+                    type = "text"
+                    value = {username}
+                    onKeyDown={(e) => {if(e.key == 'Enter') register(e)}}
+                    onChange = {handleChange}
+                ></input>
+                <span>Password</span>
+                <input
+                    className='password'
+                    name = "password"
+                    type = "text"
+                    value = {password}
+                    onKeyDown={(e) => {if(e.key == 'Enter') register(e)} }
+                    onChange = {handleChange}
+                ></input>
+            <button
+                onClick = {register}
+            >Register
+            </button>
+                </div>
+                <div className='login'>
+                    <span>Username</span>
+                    <input
+                        className = "username"
+                        name = "username"
+                        type = "text"
+                        value = {username}
+                        onKeyDown={(e) => {if(e.key == 'Enter') login(e)}}
+                        onChange = {handleChange}
+                    ></input>
+                    <span>Password</span>
+                    <input
+                        className='password'
+                        name = "password"
+                        type = "text"
+                        value = {password}
+                        onKeyDown={(e) => {if(e.key == 'Enter') login(e)} }
+                        onChange = {handleChange}
+                    ></input>
+                <button
+                    onClick = {login}
+                >login
+                </button>
+            </div>
             <button
                 onClick = {sendRequest}
             >Send http request
