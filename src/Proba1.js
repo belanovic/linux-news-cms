e1df4cf4f1312aa491424dd6e849af9b047cde27
belanovic <goranbelanovic@yahoo.com>
2023-12-27 13:38:12 +0100

    msg

--- src/Proba1.js
+++ src/Proba1.js
@@ -18,7 +18,11 @@ export default function Proba1() {
     const [loggedIn, setLoggedIn] = useState(false);
     
     const [query, setQuery] = useState('');
-    const [film, setFilm] = useState('');
+
+    const [title, setTitle] = useState('');
+    const [genre, setGenre] = useState('');
+    const [year, setYear] = useState('');
+
 
     const handleChange = (e) => {
         const value = e.target.value;
@@ -27,13 +31,19 @@ export default function Proba1() {
         if(name == 'passwordRegister') setPasswordRegister(value)
         if(name == 'usernameLogin') setUsernameLogin(value)
         if(name == 'passwordLogin') setPasswordLogin(value)
-        if(name == 'query') setQuery(value)
+        if(name == 'queryInput') setQuery(value)
+        if(name == 'titleInput') setTitle(value)
+        if(name == 'genreInput') setGenre(value)
+        if(name == 'yearInput') setYear(value)
     }
 
     const searchFilm = async () => {
         const options = {
             method: 'POST',
             credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({
                 title: query
             })
@@ -45,16 +55,83 @@ export default function Proba1() {
                 logout();
                 return
             }
-            let res_body = await res.json();
-            if(res_body.error) {
-                alert(res_body.error.message)
+            let resBody = await res.json();
+            if(resBody.error) {
+                alert(resBody.error.message)
                 return
             }
-            setOdgovor(res_body);
+            if(!resBody.queryMsg.isSuccess) {
+                alert(resBody.queryMsg.failureMsg)
+                return
+            }
+            setOdgovor([resBody.queryMsg.filmFound]);
+            setQuery('')
         } catch (error) {
             alert(error.message);
         }
     }
+
+    const getAllFilms = async () => {
+        const options = {
+            method: 'GET',
+            credentials: 'include'
+        }
+        const res = await fetch('http://localhost:2000/allFilms', options);
+        if(res.status == 401) {
+            alert('401 - Authentication error');
+            logout();
+            return
+        }
+        const resBody = await res.json();
+
+        if(resBody.error) {
+            alert(resBody.error.message);
+            return
+        }
+        if(resBody.queryMsg.isSuccess == false) {
+            alert(resBody.queryMsg.failureMsg);
+            return
+        }
+        setOdgovor(resBody.queryMsg.filmsFound)
+    }
+
+    const postFilm = async () => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                title: title,
+                genre: 'action',
+                year: 1994
+            })
+        }
+        try {
+            const res = await fetch('http://localhost:2000/postFilm', options);
+            if(res.status == 401) {
+                alert('401 - Authentication error');
+                logout();
+                return
+            }
+
+            const resBody = await res.json();
+
+            if(resBody.error) {
+                alert(resBody.error.message)
+                return
+            }
+            if(resBody.postMsg.isSaved == false) {
+                alert(resBody.postMsg.failureMsg);
+                return
+            }
+            alert(`The film ${resBody.postMsg.filmSaved.title} is saved in the database`);
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
     const register = async (e) => {
         const options = {
             method: 'POST',
@@ -68,16 +145,16 @@ export default function Proba1() {
         }
         try {
             const res = await fetch('http://localhost:2000/register', options);
-            const res_body = await res.json();
-            if(res_body.error) {
-                alert(res_body.error.message);
+            const resBody = await res.json();
+            if(resBody.error) {
+                alert(resBody.error.message);
                 return
             }
-            if(res_body.registration_msg.registered == false) {
-                alert(res_body.registration_msg.failure_msg);
+            if(resBody.registration_msg.registered == false) {
+                alert(resBody.registration_msg.failureMsg);
                 return
             }
-            alert('You are registered as ' + res_body.registration_msg.username);
+            alert('You are registered as ' + resBody.registration_msg.username);
             return
             
         } catch (error) {
@@ -99,16 +176,16 @@ export default function Proba1() {
         };
         try {
             const res = await fetch('http://localhost:2000/login', options);
-            const res_body = await res.json();
-            if(res_body.error) {
-                alert(res_body.error.message);
+            const resBody = await res.json();
+            if(resBody.error) {
+                alert(resBody.error.message);
                 return
             }
-            if(res_body.login_msg.logged_in == false) {
-                alert(res_body.login_msg.failure_msg);
+            if(resBody.login_msg.logged_in == false) {
+                alert(resBody.login_msg.failureMsg);
                 return
             }
-            alert(`You are logged in as ${res_body.login_msg.username}`);
+            alert(`You are logged in as ${resBody.login_msg.username}`);
             setLoggedIn(true);
             setUsernameLogin('');
             setPasswordLogin('');
@@ -119,11 +196,12 @@ export default function Proba1() {
         
     }
     const logout = () => {
-        const removed = cookies.remove('token', {sameSite: true});
+        const removed = cookies.remove('token', {sameSite: true, secure: true});
         setLoggedIn(false);
         setUsernameLogin('');
         setPasswordLogin('');
-        setOdgovor([])
+        setOdgovor([]);
+        setTitle('')
     }
 
     /* console.log('pozvana komponenta funkcija') */
@@ -138,86 +216,154 @@ export default function Proba1() {
             background: "black"
         }}>
             <h1>Proba1</h1>
-            <div className='register'>
-                <span>Username</span>
-                <input
-                    className = "username"
-                    name = "usernameRegister"
-                    type = "text"
-                    value = {usernameRegister}
-                    onKeyDown={(e) => {if(e.key == 'Enter') register(e)}}
-                    onChange = {handleChange}
-                    
-                    
-                ></input>
-                <span>Password</span>
-                <input
-                    className='password'
-                    name = "passwordRegister"
-                    type = "text"
-                    value = {passwordRegister}
-                    onKeyDown={(e) => {if(e.key == 'Enter') register(e)} }
-                    onChange = {handleChange}
-                    
-                ></input>
-            <button
-                onClick = {register}
-            >Register
-            </button>
+           
+            <div
+                className='visitor'
+            >
+                {
+                loggedIn?
+                <div 
+                    className='user'
+                >
+                    <div
+                        className='logout'
+                    >
+                        <button
+                            onClick = {logout}
+                        >logout
+                        </button>
+                        <span>{jwtDecode(cookies.get('token')).username}</span>
+                    </div>
                 </div>
-                <div className='login'>
-                    <span>Username</span>
-                    <input
-                        className = "username"
-                        name = "usernameLogin"
-                        type = "text"
-                        value = {usernameLogin}
-                        onKeyDown={(e) => {if(e.key == 'Enter') login(e)}}
-                        onChange = {handleChange}
-                        
-                    ></input>
-                    <span>Password</span>
-                    <input
-                        className='password'
-                        name = "passwordLogin"
-                        type = "text"
-                        value = {passwordLogin}
-                        onKeyDown={(e) => {if(e.key == 'Enter') login(e)} }
-                        onChange = {handleChange}
-                        
-                    ></input>
-                <button
-                    onClick = {() => loggedIn? logout() : login()}
-                >{loggedIn? 'logout' : 'login'}
-                </button>
-                {loggedIn?
-                <span>{jwtDecode(cookies.get('token')).username}</span>
-                :
-                ''
+                    :
+                <div
+                    className='guest'
+                >
+                    <div className='register'>
+                        <span>Username</span>
+                        <input
+                            className = "username"
+                            name = "usernameRegister"
+                            type = "text"
+                            value = {usernameRegister}
+                            onKeyDown={(e) => {if(e.key == 'Enter') register(e)}}
+                            onChange = {handleChange}
+                            
+                            
+                        ></input>
+                        <span>Password</span>
+                        <input
+                            className='password'
+                            name = "passwordRegister"
+                            type = "text"
+                            value = {passwordRegister}
+                            onKeyDown={(e) => {if(e.key == 'Enter') register(e)} }
+                            onChange = {handleChange}
+                            
+                        ></input>
+                        <button
+                            onClick = {register}
+                        >Register
+                        </button>
+                    </div>
+                    <div className='login'>
+                        <span>Username</span>
+                        <input
+                            className = "username"
+                            name = "usernameLogin"
+                            type = "text"
+                            value = {usernameLogin}
+                            onKeyDown={(e) => {if(e.key == 'Enter') login(e)}}
+                            onChange = {handleChange}
+                            
+                        ></input>
+                        <span>Password</span>
+                        <input
+                            className='password'
+                            name = "passwordLogin"
+                            type = "text"
+                            value = {passwordLogin}
+                            onKeyDown={(e) => {if(e.key == 'Enter') login(e)} }
+                            onChange = {handleChange}
+                            
+                        ></input>
+                        <button
+                            onClick = {login}
+                        >login
+                        </button>
+                    </div>
+                </div>
                 }
             </div>
-            {loggedIn?
-            <div>
+            {
+            loggedIn?
+            <div
+                className = "query"
+            >
                 <input
-                    className = "query"
-                    name = "query"
+                    className = "queryInput"
+                    name = "queryInput"
                     type = "text"
                     value = {query}
                     onChange= {handleChange}
                     onKeyDown={(e) => {if(e.key == 'Enter') searchFilm(e)}}
 
                 ></input>
-                <button
+               <button
                     onClick = {searchFilm}
-                >Search Film
+                    >Search Film
                 </button>
-                <div>{odgovor.map((prom, i) => <li key = {i}>{prom.title}</li>)}</div>
+               <button
+                    onClick = {getAllFilms}
+                    >Get all films
+                </button>
+                <div>{odgovor.map((prom, i) => <li key = {i}>{`${prom.title} is an ${prom.genre} film, released in the year ${prom.year}`}</li>)}</div>
+
             </div>
             :
             ''
             }
-
-            
+            {
+            loggedIn?
+            <div
+                className='post'
+            >   
+                <div
+                    className = 'postInput'
+                >     <label>Title</label>
+                      <input
+                        className = 'titleInput'
+                        name = 'titleInput'
+                        value = {title}
+                        onChange = {handleChange} 
+                        onKeyDown={(e) => {if(e.key == 'Enter') postFilm(e)}}
+                    ></input>
+                    <label>Genre</label>
+                    <input
+                        className = 'genreInput'
+                        name = 'genreInput'
+                        value = {genre}
+                        onChange={handleChange}
+                        onKeyDown={(e) => {if(e.key == 'Enter') postFilm(e)}}
+                    ></input>
+                    <label>Year</label>
+                    <input
+                        className = 'yearInput'
+                        name = 'yearInput'
+                        value = {year}
+                        onChange={handleChange}
+                        onKeyDown={(e) => {if(e.key == 'Enter') postFilm(e)}}
+                    ></input>
+                </div>
+                <button
+                    className = 'postBtn'
+                    onClick={postFilm}
+                >Post film
+                </button>
+            </div>
+            :
+            ''
+            }
         </div> 
     )
 }
